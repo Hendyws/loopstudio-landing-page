6cd8ee542a2948ed4183cc36669873e6aa8946e3
Henderson Abreu <henderson.abreu.silva@gmail.com>
2022-03-23 00:13:10 -0300

    delet all files

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const btnMenu = document.querySelector('.btn-menu');
-function toggleMenu () {
-    const navMenu = document.querySelector('.menu');
-    navMenu.classList.toggle('active');
-}
-btnMenu.addEventListener('click', toggleMenu);
